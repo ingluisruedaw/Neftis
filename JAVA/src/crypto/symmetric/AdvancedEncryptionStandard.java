18acdb6e354dc19721ffbeec0be4b37ab4ebb398
Luis Rueda <ingluisruedaw@gmail.com>
2023-06-28 07:42:18 -0500

    Remove necesary code

--- JAVA/src/crypto/symmetric/AdvancedEncryptionStandard.java
+++ JAVA/src/crypto/symmetric/AdvancedEncryptionStandard.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package crypto.symmetric;
 
 import javax.crypto.Cipher;
